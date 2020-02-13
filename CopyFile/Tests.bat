90ad28d4775306428b161d3141833a2895f0bdd7
Alexey Malov <malov.aleksey@gmail.com>
2020-02-13 23:27:41 +0300

    Добавлены недостающие тесты

--- CopyFile/Tests.bat
+++ CopyFile/Tests.bat
@@ -11,9 +11,28 @@ if %MyProgram%=="" (
 
 REM Copy empty file
 %MyProgram% Empty.txt "%TEMP%\output.txt" || goto err
-fc Empty.txt "%TEMP%\output.txt" || goto err
+fc Empty.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 1 passed
+
+REM Copy non empty file
+%MyProgram% NonEmptyFile.txt "%TEMP%\output.txt" || goto err
+fc NonEmptyFile.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 2 passed
+
+REM Copy missing file should fail
+%MyProgram% MissingFile.txt "%TEMP%\output.txt" && goto err
+echo Test 3 passed
+
+REM If output file is not specified, program must fail
+%MyProgram% MissingFile.txt && goto err
+echo Test 4 passed
+
+REM If input and output files are not specified, program must fail
+%MyProgram% && goto err
+echo Test 5 passed
 
 REM Тесты прошли успешно
+echo All tests passed successfuly
 exit /B 0
 
 REM Сюда будем попадать в случае ошибки
