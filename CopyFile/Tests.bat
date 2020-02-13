ca206fb39b0bdc71ee3d8085fa40d3e258db8be9
Alexey Malov <malov.aleksey@gmail.com>
2020-02-13 23:14:29 +0300

    добавлен тест

---
+++ CopyFile/Tests.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Путь к тестируемой программе передаётся через 1-й агрумент командной строки
+SET MyProgram="%~1"
+
+REM Защита от запуска без агрумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /B 1
+)
+
+REM Copy empty file
+%MyProgram% Empty.txt "%TEMP%\output.txt" || goto err
+fc Empty.txt "%TEMP%\output.txt" || goto err
+
+REM Тесты прошли успешно
+exit /B 0
+
+REM Сюда будем попадать в случае ошибки
+:err
+echo Test failed
+exit /B 1
